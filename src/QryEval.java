3dcca992b8542b9506cb22a6747bfcf80fffc906
KyleMao <maozeximzx@gmail.com>
2015-04-10 15:11:58 -0400

    First commit

---
+++ src/QryEval.java
@@ -0,0 +1,19 @@
+/**
+ * QryEval illustrates the architecture for the portion of a search engine that evaluates queries
+ * for a LeToR system.
+ * 
+ * @author KyleMao
+ *
+ */
+
+
+public class QryEval {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("test");
+  }
+
+}
